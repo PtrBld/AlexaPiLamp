3744acf6007f3e03160a2654f3bc3cfe45f2d963
PtrBld <peter-bludau@gmx.de>
2017-08-20 11:59:58 +0200

    changed

--- AlexaPiLamp_WebApp/app.py
+++ AlexaPiLamp_WebApp/app.py
@@ -6,11 +6,6 @@ from flask import Flask, render_template, request
 import urllib.parse
 from threading import Thread
 
-from display.disp7seg import DisplayThread
-from audio.alarm import AlarmThread
-from gpio.btn import ButtonThread
-from ledstrip.apa102 import ApaStripThread
-
 app = Flask(__name__, template_folder="./templates",
             static_url_path="/static",
             static_folder="./static")
@@ -24,49 +19,29 @@ def home():
 
 @app.route('/togglelight', methods=['POST'])
 def toggle_light():
-    _apa_strip.toggle_lights()
+    res = requests.get("http://192.168.178.33:8080/apa102?sequence=toggle")
+    print('response from server:', res.text)
     return json.dumps({'status': 'OK'})
 
 
 @app.route('/setlightcolor', methods=['POST'])
 def set_light_color():
-    _apa_strip.set_color(colors.getIfromRGB(tuple(request.json)))
+    res = requests.get("http://192.168.178.33:8080/apa102?color=" + str(tuple(request.json)))
+    print('response from server:', res.text)
     return json.dumps({'status': 'OK'})
-
-
-@app.route('/setbrightness', methods=['POST'])
-def set_brightness():
-    _apa_strip.set_brightness(int(request.json))
-    return json.dumps({'status': 'OK'})
-
 
 @app.route('/setleds', methods=['POST'])
 def set_leds():
-    _apa_strip.set_leds(int(request.json))
+    res = requests.get("http://192.168.178.33:8080/apa102?leds=" + str(int(request.json)))
+    print('response from server:', res.text)
     return json.dumps({'status': 'OK'})
 
 @app.route('/setmodus', methods=['POST'])
 def set_modus():
-    _apa_strip.set_modus(str(request.json))
+    res = requests.get("http://192.168.178.33:8080/apa102?sequence=" + str(request.json))
+    print('response from server:', res.text)
     return json.dumps({'status': 'OK'})
-
-
-@app.route('/setalarm', methods=['POST'])
-def set_alarm():
-    _alarm.set_alarm(str(request.json))
-    return json.dumps({'status': 'OK'})
-
-
-_display = DisplayThread()
-_button = ButtonThread(17)
-_apa_strip = ApaStripThread(430)
-_alarm = AlarmThread()
 
 if __name__ == "__main__":
-    # instantiate modules
-    _display.start()
-    _button.start()
-    _apa_strip.start()
-    _alarm.start()
     # run flask app
     app.run()
